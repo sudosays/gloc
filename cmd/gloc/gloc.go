053a3b987cc3adf3ff010c2e392569bc1f1ebdf5
Julius Stopforth <jjstopforth@gmail.com>
2020-11-10 18:23:04 +0200

    Add interactive flag that redraws output

--- cmd/gloc/gloc.go
+++ cmd/gloc/gloc.go
@@ -1,32 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "time"
-//    "github.com/sudosays/gloc/pkg/convert"
-    "github.com/sudosays/gloc/pkg/localise"
+	"flag"
+	"fmt"
+	"time"
+	//    "github.com/sudosays/gloc/pkg/convert"
+	"github.com/sudosays/gloc/pkg/localise"
 )
 
 var interactive bool
 
-func init() { 
-    flag.BoolVar(&interactive, "i", false, "Run in interactive mode, continuously updating calculations")
+func init() {
+	flag.BoolVar(&interactive, "i", false, "Run in interactive mode, continuously updating calculations")
 }
 
 func main() {
-    
-    flag.Parse()
+	longitude := 22.45
+	//latitude := -33.966667
+	flag.Parse()
+	fmt.Printf("hello, universe!\n\n")
+	localSiderealTime := localise.CalculateCurrentSiderealTimeFromEpoch(longitude)
+	if interactive {
+		for {
+			EraseOutputLine()
+			localSiderealTime = localise.CalculateCurrentSiderealTimeFromEpoch(longitude)
+			fmt.Println("Current sidereal time is ", localSiderealTime, " degrees.")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 
-    fmt.Printf("hello, universe!\n")
+}
 
-    localise.CalculateCurrentSiderealTimeFromEpoch()
-    
-    if (interactive) {
-        for {
-            fmt.Print("\u001b[F\u001b[0K")
-            localise.CalculateCurrentSiderealTimeFromEpoch()
-            time.Sleep(50 * time.Millisecond)
-        }
-    }
+func EraseOutputLine() {
+	fmt.Print("\u001b[F\u001b[0K")
 }
