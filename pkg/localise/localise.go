69f75f3c23c257f9c2d43e0b8562b45f821be3d0
Julius Stopforth <jjstopforth@gmail.com>
2020-11-10 18:24:12 +0200

    Add local sidereal time calculation

--- pkg/localise/localise.go
+++ pkg/localise/localise.go
@@ -2,8 +2,8 @@
 package localise
 
 import (
-    "fmt"
     "time"
+    "math"
 )
 
 // Specified according to RFC3339 as a string to allow readability
@@ -11,15 +11,33 @@ const EpochTime = "2000-01-01T12:00:00.00Z"
 
 // Geographical coordinates assume decimal format. 
 // TODO Add contversion of deg min sec to decimal in convert pkg
+// East positive
+// North positive
+// GeographicalCoordinates are inn Decimal degrees (DD)
 type GeographicalCoordinates struct {
-    Latitude, Lontitude float64
+    Latitude, Longitude float64
 }
 
-func CalculateCurrentSiderealTimeFromEpoch() {
+// CalculateCurrentSiderealTimeFromEpoch gives the current sidereal time in
+// degrees at the given longitude as of time.Now().
+func CalculateCurrentSiderealTimeFromEpoch(longitude float64) float64 {
     epoch, _ := time.Parse(time.RFC3339, EpochTime)
     t := time.Now()
 
+    utc := float64(t.UTC().Hour()) + float64(t.UTC().Minute())/1440 + float64(t.UTC().Second())/86400
+
     diff := t.Sub(epoch)
 
-    fmt.Println("Time since epoch J2000: ", diff)
+    decimalDays := diff.Hours()/24
+
+    localSiderealTime := 100.46 + 0.985647 * decimalDays + longitude + float64(15)*utc
+    localSiderealTime = math.Mod(localSiderealTime, 360)
+
+    if (localSiderealTime < 0) {
+       localSiderealTime += 360
+    }
+
+    return localSiderealTime
 }
+
+
