b9513a1b9013aaade27da017fb9faf3b53888ba8
Julius Stopforth <jjstopforth@gmail.com>
2021-01-11 15:53:10 +0200

    Add local sidereal time approximate calculation
    The localise package now correctly calculates the mean sidereal time for
    a given longitude. The equation used is an approximation, but accurate
    enough for now.

--- pkg/localise/localise.go
+++ pkg/localise/localise.go
@@ -1,46 +1,55 @@
-// Package for the terra-celestial localisation 
+// Package for the terra-celestial localisation
 package localise
 
 import (
-    "time"
-    "math"
+	"math"
+	"time"
 )
 
 // Specified according to RFC3339 as a string to allow readability
 const EpochTime = "2000-01-01T12:00:00.00Z"
 
-// Geographical coordinates assume decimal format. 
+// Geographical coordinates assume decimal format.
 // TODO Add contversion of deg min sec to decimal in convert pkg
 // East positive
 // North positive
 // GeographicalCoordinates are inn Decimal degrees (DD)
 type GeographicalCoordinates struct {
-    Latitude, Longitude float64
+	Latitude, Longitude float64
 }
 
 // CalculateCurrentSiderealTimeFromEpoch gives the current sidereal time in
 // degrees at the given longitude as of time.Now().
 func CalculateCurrentSiderealTimeFromEpoch(longitude float64) float64 {
-    decimalDays := DecimalDaysSinceEpoch(time.Now())
+	ut := time.Now().UTC()
+	decimalDays := DecimalDaysSinceEpoch(ut)
 
-    localSiderealTime := 100.46 + 0.985647 * decimalDays + longitude
-    localSiderealTime = math.Mod(localSiderealTime, 360)
+	var t float64
+	t = float64(ut.Hour())
+	t += float64(ut.Minute()) / 60.0
+	t += float64(ut.Second()) / 3600.0
 
-    if (localSiderealTime < 0) {
-       localSiderealTime += 360
-    }
+	localSiderealTime := 100.46 + 0.985647*decimalDays
+	localSiderealTime += longitude
+	localSiderealTime += 15.0 * t
 
-    return localSiderealTime
+	localSiderealTime = math.Mod(localSiderealTime, 360)
+
+	if localSiderealTime < 0 {
+		localSiderealTime += 360
+	}
+
+	return localSiderealTime
 }
 
 func DecimalDaysSinceEpoch(t time.Time) float64 {
 
-    epoch, _ := time.Parse(time.RFC3339, EpochTime)
+	epoch, _ := time.Parse(time.RFC3339, EpochTime)
 
-    diff := t.Sub(epoch)
+	diff := t.Sub(epoch)
 
-    decimalDays := diff.Hours()/24
+	decimalDays := diff.Hours() / 24
 
-    return decimalDays
+	return decimalDays
 
 }
